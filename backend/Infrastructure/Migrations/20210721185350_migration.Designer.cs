f0a1c81e462b547fa7c9d528b151dbcf23260123
HueByte <ainodeve@gmail.com>
2021-07-21 22:14:03 +0200

    Too many to list

--- backend/Infrastructure/Migrations/20210721185350_migration.Designer.cs
+++ backend/Infrastructure/Migrations/20210721185350_migration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210721185350_migration")]
+    partial class migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- backend/Infrastructure/Repositories/CategoryRepository.cs
+++ backend/Infrastructure/Repositories/CategoryRepository.cs
@@ -5,21 +5,24 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Core.Models;
 using Core.RepositoriesInterfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
-    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
-        readonly AppDbContext _context;
-        public CategoryRepository(AppDbContext context)
+        private readonly AppDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public CategoryRepository(AppDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public override async Task<Category> GetOneAsync(string name)
+        public async Task<Category> GetOneAsync(string name, string ownerId)
         {
-            var category = await _context.Categories.FirstOrDefaultAsync(cat => cat.name == name);
+            var category = await _context.Categories.FirstOrDefaultAsync(cat => cat.Name == name && cat.Owner.Id == ownerId);
 
             if (category == null)
                 throw new Exception("Couldn't find this category");
@@ -27,30 +30,33 @@ namespace Infrastructure.Repositories
             return category;
         }
 
-        public override async Task<List<Category>> GetAllAsync()
+        public async Task<List<Category>> GetAllAsync(string ownerId)
         {
-            var categories = await _context.Categories.ToListAsync();
+            var categories = await _context.Categories.Where(cat => cat.Owner.Id == ownerId).ToListAsync();
             if (categories.Count == 0 || categories == null)
                 throw new Exception("Couldn't find any categories");
 
             return categories;
         }
 
-        public override async Task AddOneAsync(Category cat)
+        public async Task AddOneAsync(Category cat, string ownerName)
         {
-            if (string.IsNullOrWhiteSpace(cat.name))
+            var user = await _userManager.FindByNameAsync(ownerName);
+            if (string.IsNullOrWhiteSpace(cat.Name))
                 throw new ArgumentException("Name cannot be empty");
+
+            cat.Owner = user ?? throw new Exception("Something went wrong");
 
             await _context.Categories.AddAsync(cat);
             await _context.SaveChangesAsync();
         }
 
-        public override async Task RemoveOneAsync(string name)
+        public async Task RemoveOneAsync(string name, string ownerId)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentException("Name cannot be empty");
 
-            var category = await _context.Categories.FirstOrDefaultAsync(x => x.name == name);
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Name == name && x.Owner.Id == ownerId);
             if (category == null)
                 throw new Exception("Couldn't find that category");
 
