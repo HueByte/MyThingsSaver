969755ddbbfa4828cd2b255f17ae63975ac68042
HueByte <ainodeve@gmail.com>
2021-07-31 16:09:35 +0200

    cleaning

--- client/src/routes/AuthenticatedRoute.jsx
+++ client/src/routes/AuthenticatedRoute.jsx
@@ -4,17 +4,8 @@ import { AuthContext } from '../auth/AuthContext'
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
     const authContext = useContext(AuthContext);
-
-    useEffect(() => console.log('Private hit'));
     return (
         <Route {...rest} render={props => {
-            // if (!authContext.isAuthenticated()) {
-            //     return <Redirect to="/auth/login" />
-            // }
-
-            // return <Component {...props} />
-            if (authContext.isAuthenticated()) console.log('authenticated');
-
             return authContext.isAuthenticated() ? <Component {...props} /> : <Redirect to="/auth/login" />
         }} />
     )
