e2c24e0d0a34a3868ba57587dacb92837f6e7849
HueByte <ainodeve@gmail.com>
2021-08-07 12:14:03 +0200

    BaseURL location idea

--- client/src/routes/ApiEndpoints.js
+++ client/src/routes/ApiEndpoints.js
@@ -1,4 +1,5 @@
-export const BaseUrl = process.env.REACT_APP_API_URL ?? 'https://localhost:5001/';
+// export const BaseUrl = process.env.REACT_APP_API_URL ?? 'https://localhost:5001/';
+export const BaseUrl = `https://${window.location.host}/`;
 
 // api/auth
 export const RegisterEndpoint = `${BaseUrl}api/Authenticate/register`;
