023a9f074b1623a927349f06b6624841cfe5c1d4
HueByte <ainodeve@gmail.com>
2021-08-10 15:13:09 +0200

    Changed placement in controller

--- backend/App/Controllers/CategoryEntryController.cs
+++ backend/App/Controllers/CategoryEntryController.cs
@@ -20,36 +20,6 @@ namespace App.Controllers
             _categoryEntryRepository = categoryEntryRepository;
         }
 
-        [HttpPost("AddEntry")]
-        [Authorize]
-        public async Task<IActionResult> AddEntryAsync([FromBody] CategoryEntryDTO entry)
-        {
-            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            var result = await ApiEventHandler.EventHandleAsync(async () =>
-                await _categoryEntryRepository.AddOneAsync(entry, userId));
-
-            if (result.IsSuccess)
-                return Ok(result);
-            else
-                return BadRequest(result);
-        }
-
-        [HttpGet("GetEntryByName")]
-        [Authorize]
-        public async Task<IActionResult> GetEntryByNameAsync(string categoryId, string name)
-        {
-            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            var result = await ApiEventHandler<CategoryEntry>.EventHandleAsync(async () =>
-            {
-                return await _categoryEntryRepository.GetOneByNameAsync(categoryId, name, userId);
-            });
-
-            if (result.IsSuccess)
-                return Ok(result);
-            else
-                return BadRequest(result);
-        }
-
         [HttpGet("GetEntryById")]
         [Authorize]
         public async Task<IActionResult> GetEntryByIdAsync(string id)
@@ -75,6 +45,20 @@ namespace App.Controllers
             {
                 return await _categoryEntryRepository.GetAllAsync(categoryId, userId, withContent);
             });
+
+            if (result.IsSuccess)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+
+        [HttpPost("AddEntry")]
+        [Authorize]
+        public async Task<IActionResult> AddEntryAsync([FromBody] CategoryEntryDTO entry)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = await ApiEventHandler.EventHandleAsync(async () =>
+                await _categoryEntryRepository.AddOneAsync(entry, userId));
 
             if (result.IsSuccess)
                 return Ok(result);
