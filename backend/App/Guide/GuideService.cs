17119e36bc1fb03c7fec0af2d32d9ee6b174e163
HueByte <ainodeve@gmail.com>
2021-09-06 17:25:19 +0200

    calculate size on load (guide)

--- backend/App/Guide/GuideService.cs
+++ backend/App/Guide/GuideService.cs
@@ -1,12 +1,15 @@
 using System;
 using System.IO;
+using System.Text;
 
 namespace App.Guide
 {
     public class GuideService
     {
         public string WELCOME { get; set; }
+        public int WELCOME_SIZE { get; set; }
         public string GUIDE { get; set; }
+        public int GUIDE_SIZE { get; set; }
 
         private static string FolderPath
             => AppContext.BaseDirectory + @"\Guide\Templates";
@@ -15,6 +18,8 @@ namespace App.Guide
         {
             WELCOME = File.ReadAllText(Path.Combine(FolderPath, "Welcome.md"));
             GUIDE = File.ReadAllText(Path.Combine(FolderPath, "Guide.md"));
+            WELCOME_SIZE = ASCIIEncoding.Unicode.GetByteCount(WELCOME);
+            GUIDE_SIZE = ASCIIEncoding.Unicode.GetByteCount(GUIDE);
         }
     }
 }
