a57d637eecf3d30a4df7af8c0f73ca3a7afe2b6f
HueByte <ainodeve@gmail.com>
2022-01-08 02:39:34 +0100

    Working on resizable explorer

--- backend/App/client/src/pages/Explorer/Explorer.jsx
+++ backend/App/client/src/pages/Explorer/Explorer.jsx
@@ -5,7 +5,7 @@ const Explorer = () => {
   return (
     <div className="categories__wrapper">
       <div className="container">
-        <div className="left-menu">
+        <div className="left-menu border-gradient border-gradient-purple">
           <div className="item ellipsis">cat</div>
           <div className="item ellipsis">category something something</div>
           <div className="item ellipsis">category something something</div>
@@ -19,21 +19,85 @@ const Explorer = () => {
         </div>
         <div className="content">
           <div className="item">
-            <div className="icon">
-              <i class="fa fa-folder" aria-hidden="true"></i>
+            <div className="information">
+              <div className="icon">
+                <i class="fa fa-folder" aria-hidden="true"></i>
+              </div>
+              <div className="text ellipsis">
+                I'm some item reeeeeeeeeeeeeeeeeeeeeeeee
+              </div>
+              <div className="date">11/11/2020</div>
+              <div className="size">100 B</div>
             </div>
-            <div className="text ellipsis">I'm some item</div>
-            <div className="date">11/11/2020</div>
-            <div className="size">100 B</div>
             <div className="actions">
               <i class="fas fa-pen-square"></i>
               <i class="fa fa-times" aria-hidden="true"></i>
             </div>
           </div>
-          <div className="item ellipsis">I'm some item</div>
-          <div className="item ellipsis">I'm some item</div>
-          <div className="item ellipsis">I'm some item</div>
-          <div className="item ellipsis">I'm some item</div>
+          <div className="item">
+            <div className="information">
+              <div className="icon">
+                <i class="fas fa-sticky-note"></i>
+              </div>
+              <div className="text ellipsis">
+                I'm some item reeeeeeeeeeeeeeeeeeeeeeeee
+              </div>
+              <div className="date">11/11/2020</div>
+              <div className="size">100 B</div>
+            </div>
+            <div className="actions">
+              <i class="fas fa-pen-square"></i>
+              <i class="fa fa-times" aria-hidden="true"></i>
+            </div>
+          </div>
+          <div className="item">
+            <div className="information">
+              <div className="icon">
+                <i class="fa fa-folder" aria-hidden="true"></i>
+              </div>
+              <div className="text ellipsis">
+                I'm some item reeeeeeeeeeeeeeeeeeeeeeeee
+              </div>
+              <div className="date">11/11/2020</div>
+              <div className="size">100 B</div>
+            </div>
+            <div className="actions">
+              <i class="fas fa-pen-square"></i>
+              <i class="fa fa-times" aria-hidden="true"></i>
+            </div>
+          </div>
+          <div className="item">
+            <div className="information">
+              <div className="icon">
+                <i class="fas fa-sticky-note"></i>
+              </div>
+              <div className="text ellipsis">
+                I'm some item reeeeeeeeeeeeeeeeeeeeeeeee
+              </div>
+              <div className="date">11/11/2020</div>
+              <div className="size">100 B</div>
+            </div>
+            <div className="actions">
+              <i class="fas fa-pen-square"></i>
+              <i class="fa fa-times" aria-hidden="true"></i>
+            </div>
+          </div>
+          <div className="item">
+            <div className="information">
+              <div className="icon">
+                <i class="fa fa-folder" aria-hidden="true"></i>
+              </div>
+              <div className="text ellipsis">
+                I'm some item reeeeeeeeeeeeeeeeeeeeeeeee
+              </div>
+              <div className="date">11/11/2020</div>
+              <div className="size">100 B</div>
+            </div>
+            <div className="actions">
+              <i class="fas fa-pen-square"></i>
+              <i class="fa fa-times" aria-hidden="true"></i>
+            </div>
+          </div>
         </div>
       </div>
     </div>
