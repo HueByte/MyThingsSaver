56b9864d0ba90f4e59c4178f5d070cda39eed0c6
HueByte <ainodeve@gmail.com>
2022-06-30 19:53:58 +0200

    Updated BaseApiResponse

--- src/Common/ApiResonse/BaseApiResponse.cs
+++ src/Common/ApiResonse/BaseApiResponse.cs
@@ -1,11 +1,26 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Common.ApiResonse
 {
-    public class BaseApiResponse<T> 
+    public class BaseApiResponse<T>
     {
+        [JsonPropertyName("data")]
         public T? Data { get; set; }
-        public List<string> Errors { get; set; }
+
+        [JsonPropertyName("errors")]
+        public List<string?>? Errors { get; set; }
+
+        [JsonPropertyName("isSuccess")]
         public bool IsSuccess { get; set; }
+
+        public BaseApiResponse() { }
+        public BaseApiResponse(T? data) : this(data, null, true) { }
+        public BaseApiResponse(T? data, List<string> errors, bool isSuccess)
+        {
+            Data = data;
+            Errors = errors!;
+            IsSuccess = isSuccess;
+        }
     }
 }
