30f3429b646510bd50dd9bccff91a6e263480503
huebyte <ihuebyte@gmail.com>
2022-12-01 02:39:26 +0100

    New background

--- src/App/client/src/index.js
+++ src/App/client/src/index.js
@@ -5,8 +5,10 @@ import App from "./App";
 import "./index.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import bg from "./assets/Sprinkle.svg";
 
 const container = document.getElementById("root");
+container.style.backgroundImage = `url(${bg}`;
 const root = createRoot(container);
 root.render(<App />);
 
