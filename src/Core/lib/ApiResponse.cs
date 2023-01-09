e5628af50efdd20dafbd7edbe0a103f5e9b6dbe7
huebyte <ihuebyte@gmail.com>
2023-01-09 12:55:54 +0100

    Renames for api response

--- src/Core/lib/ApiResponse.cs
+++ src/Core/lib/ApiResponse.cs
@@ -4,7 +4,7 @@ using MTS.Common.ApiResonse;
 
 namespace MTS.Core.lib
 {
-    public static class CreateResponse
+    public static class ApiResponse
     {
         public static IActionResult Empty()
         {
@@ -12,21 +12,21 @@ namespace MTS.Core.lib
             return new OkObjectResult(result);
         }
 
-        public static IActionResult FromData<T>(T? data) where T : class
+        public static IActionResult Data<T>(T? data) where T : class
         {
             BaseApiResponse<T> result = new(data);
-            return FromBaseApiResponse(result);
+            return Create(result);
         }
 
-        public static IActionResult FromPrimitive<T>(T data) where T : struct
+        public static IActionResult ValueType<T>(T data) where T : struct
         {
             if (!typeof(T).IsPrimitive) throw new Exception($"{data.GetType()} is not primitive data type");
 
             BaseApiResponse<T> result = new(data);
-            return FromBaseApiResponse(result);
+            return Create(result);
         }
 
-        public static IActionResult FromBaseApiResponse<T>(BaseApiResponse<T> result)
+        public static IActionResult Create<T>(BaseApiResponse<T> result)
         {
             if (result.IsSuccess)
                 return new OkObjectResult(result);
