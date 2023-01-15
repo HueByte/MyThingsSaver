a1996b626cde7d5da5baf4a31e88b2c72710be65
huebyte <ihuebyte@gmail.com>
2023-01-15 20:43:58 +0100

    admins variable in appsettings and assignment & role badges gap

--- src/App/Configuration/AppConfigurator.cs
+++ src/App/Configuration/AppConfigurator.cs
@@ -19,8 +19,10 @@ namespace MTS.App.Configuration
         public static async Task<WebApplication> SeedIdentity(this WebApplication webapp)
         {
             await using var scope = webapp.Services.CreateAsyncScope();
+            var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
             var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUserModel>>();
+
             string[] roles = { Role.USER, Role.ADMIN };
 
             foreach (var role in roles)
@@ -44,7 +46,30 @@ namespace MTS.App.Configuration
                 if (result.Succeeded) await userManager.AddToRoleAsync(admin, Role.ADMIN);
             }
 
+            var adminUsers = config.GetSection("Admins")?.Get<string[]>();
+
+            if (adminUsers is null || adminUsers.Length <= 0) return webapp;
+
+            foreach (var user in adminUsers)
+            {
+                var result = await AssignAdminRole(userManager, user);
+
+                if (result) Console.WriteLine("User {0} was assigned to admin role.", user);
+                else Console.WriteLine("User {0} failed to be assigned to admin role.", user);
+            }
+
             return webapp;
+        }
+
+        private static async Task<bool> AssignAdminRole(UserManager<ApplicationUserModel> userManager, string username)
+        {
+            var user = await userManager.FindByNameAsync(username);
+            if (user is null) return false;
+
+            if (await userManager.IsInRoleAsync(user, Role.ADMIN)) return true;
+            var result = await userManager.AddToRoleAsync(user, Role.ADMIN);
+
+            return result.Succeeded;
         }
     }
 }
