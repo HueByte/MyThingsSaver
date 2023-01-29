f07176ae2ee19553d91cd1c3574617136f5c1c61
huebyte <ihuebyte@gmail.com>
2023-01-29 21:01:47 +0100

    Account page migration to tailwind

--- src/App/client/src/pages/User/Account.jsx
+++ src/App/client/src/pages/User/Account.jsx
@@ -23,32 +23,48 @@ const AccountPage = () => {
   };
 
   return (
-    <div className="user-panel-container">
-      <div className="user-panel">
-        <div className="user-card border-gradient">
-          <div className="avatar">
-            <img src={authContext.getAvatar()} alt="avatar" />
+    <div className="user-panel-container flex justify-center">
+      <div className="user-panel flex w-[1024px] flex-row gap-6 pt-12">
+        <div className="user-card border-gradient border-1 flex h-fit w-[250px] flex-col">
+          <div className="avatar h-[250px] w-full overflow-hidden rounded-t-xl">
+            <img
+              src={authContext.getAvatar()}
+              alt="avatar"
+              className="h-full w-full rounded-full"
+            />
           </div>
           <abbr
             title={authContext.authState?.username}
-            className="username ellipsis"
+            className="username ellipsis bold text-center text-4xl font-bold tracking-wide"
           >
             {authContext.authState?.username}
           </abbr>
-          <div className="badges">
+          <div className="badges flex flex-row flex-wrap justify-center gap-4 rounded-b-xl">
             {authContext.authState.roles?.map((role, index) => (
-              <div key={index} className="badge" style={getRoleStyle()}>
+              <div
+                key={index}
+                className="badge rounded px-2 py-1"
+                style={getRoleStyle()}
+              >
                 {capitalizeRole(role)}
               </div>
             ))}
           </div>
         </div>
-        <div className="content">
-          <div className="menu">
-            <NavLink to="user/me" activeClassName="active" className="item">
+        <div className="content mb-8 flex flex-row rounded-xl bg-altBackgroundColor">
+          <div className="menu flex h-full flex-col border-r-2 border-primary p-4">
+            <NavLink
+              to="user/me"
+              activeClassName="active"
+              className="item flex items-center rounded bg-backgroundColor p-2 hover:text-primary"
+            >
               <AiOutlineUser /> <span>Me</span>
             </NavLink>
-            <NavLink to="user/logs" activeClassName="active" className="item">
+            <NavLink
+              to="user/logs"
+              activeClassName="active"
+              className="item flex items-center rounded bg-backgroundColor p-2 hover:text-primary"
+            >
               <HiOutlineClipboard /> <span>Login logs</span>
             </NavLink>
             {authContext.isInRole([Role.Admin]) ? (
@@ -56,14 +72,14 @@ const AccountPage = () => {
                 <NavLink
                   to="admin/logs"
                   activeClassName="active"
-                  className="item"
+                  className="item flex items-center rounded bg-backgroundColor p-2 hover:text-primary"
                 >
                   <AiFillFire /> <span>Admin logs</span>
                 </NavLink>
                 <NavLink
                   to="admin/usermanagement"
                   activeClassName="active"
-                  className="item"
+                  className="item flex items-center rounded bg-backgroundColor p-2 hover:text-primary"
                 >
                   <AiFillCloud /> <span>Users</span>
                 </NavLink>
@@ -72,7 +88,7 @@ const AccountPage = () => {
               <></>
             )}
           </div>
-          <div className="content-page">
+          <div className="content-page relative w-full p-4">
             <Outlet />
           </div>
         </div>
