336f75c45d186c39e39609e00511ae31c79d5641
huebyte <ihuebyte@gmail.com>
2023-01-30 13:35:55 +0100

    Base styles updated to target mainly tailwind

--- src/App/client/src/index.js
+++ src/App/client/src/index.js
@@ -1,14 +1,11 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import "./styles/main.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import bg from "./assets/Sprinkle.svg";
 
 const container = document.getElementById("root");
-container.style.backgroundImage = `url(${bg}`;
 const root = createRoot(container);
 root.render(<App />);
 
