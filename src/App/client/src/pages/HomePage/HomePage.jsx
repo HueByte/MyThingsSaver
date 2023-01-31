14baf2b73b602cb80d3174b4f3aac1526d7355cc
huebyte <ihuebyte@gmail.com>
2023-01-31 01:39:07 +0100

    New homepage

--- src/App/client/src/pages/HomePage/HomePage.jsx
+++ src/App/client/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
-import { FaFolderOpen, FaStickyNote } from "react-icons/fa";
+import { FaFolderOpen, FaGhost, FaStickyNote } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import { EntriesService } from "../../api";
 import Loader from "../../components/Loaders/Loader";
 import { getSize } from "../../core/Lib";
 import { errorModal } from "../../core/Modals";
-import "./HomePage.scss";
 
 const HomePage = () => {
   const [entries, setEntries] = useState([]);
@@ -25,73 +24,64 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="homepage__container">
-      {isFetching ? (
-        <Loader />
-      ) : (
-        <>
-          {entries.length > 0 ? (
-            entries.map((entry, index) => (
-              <NavLink
-                to={`/entry/${entry.category.id}/${entry.id}`}
-                className="entry"
-                key={index}
-              >
-                <div className="image">
-                  <FaStickyNote />
+    <>
+      <div className="m-auto flex h-full w-full max-w-[1920px] flex-row flex-wrap justify-center gap-8 px-32 py-16 lg:px-8">
+        {isFetching ? (
+          <Loader />
+        ) : (
+          <>
+            {entries.length > 0 ? (
+              entries.map((entry, index) => (
+                <NavLink
+                  to={`/entry/${entry.category.id}/${entry.id}`}
+                  className="entry"
+                  key={index}
+                >
+                  <div className="mts-border-gradient-r h-[200px] w-[250px] shadow-xl shadow-element transition duration-300 hover:text-accent7 hover:shadow-accent5">
+                    <div className="h-full w-full rounded-lg bg-altBackgroundColor p-2">
+                      <div className="text-2xl">
+                        <FaStickyNote />
+                      </div>
+                      <div className="mts-text-gradient truncate text-lg font-bold">
+                        {entry.name}
+                      </div>
+                      <div className="flex w-full flex-row gap-1 text-textColor">
+                        <div className="flex flex-1 flex-col">
+                          <div>Category</div>
+                          <div>Edited on</div>
+                          <div>Created on</div>
+                          <div>Size</div>
+                        </div>
+                        <div className="flex flex-1 flex-col">
+                          <div>{entry.category.name}</div>
+                          <div>
+                            {new Date(
+                              entry.lastUpdatedOn + "Z"
+                            ).toLocaleDateString()}
+                          </div>
+                          <div>
+                            {new Date(
+                              entry.createdOn + "Z"
+                            ).toLocaleDateString()}
+                          </div>
+                          <div>{getSize(entry.size)}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </NavLink>
+              ))
+            ) : (
+              <div className="grid h-full w-full place-items-center">
+                <div className="text-4x4 flex flex-col items-center gap-4 text-4xl">
+                  <FaGhost /> Kind of empty here
                 </div>
-                <div className="name ellipsis">{entry.name}</div>
-                <div className="information">
-                  <div className="line">
-                    <div className="item">Category:</div>
-                    <div className="item ellipsis">{entry.category.name}</div>
-                  </div>
-                  <div className="line">
-                    <div className="item">Edited on:</div>
-                    <div className="item">
-                      {new Date(entry.lastUpdatedOn + "Z").toLocaleDateString()}
-                    </div>
-                  </div>
-                  <div className="line">
-                    <div className="item">Created on:</div>
-                    <div className="item">
-                      {new Date(entry.createdOn + "Z").toLocaleDateString()}
-                    </div>
-                  </div>
-                  <div className="line">
-                    <div className="item">Size:</div>
-                    <div className="item">
-                      <div>{getSize(entry.size)}</div>
-                    </div>
-                  </div>
-                </div>
-              </NavLink>
-            ))
-          ) : (
-            <div
-              style={{
-                display: "grid",
-                placeItems: "center",
-                width: "100%",
-                height: "100%",
-                fontSize: "xxx-large",
-              }}
-            >
-              <FaFolderOpen />
-              <span
-                style={{
-                  color: "var(--Rose)",
-                  fontSize: "x-large",
-                  textAlign: "center",
-                }}
-              >
-                You haven't got any recent entries
-              </span>
-            </div>
-          )}
-        </>
-      )}
-    </div>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
